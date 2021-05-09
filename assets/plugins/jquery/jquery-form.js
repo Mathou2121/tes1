9c8114c3406de9fbfb16fe4518f8363e323c7c81
Mathieu Dion <mathieu.dion98@gmail.com>
2021-05-09 09:33:19 -0400

    first

---
+++ assets/plugins/jquery/jquery-form.js
@@ -0,0 +1,35 @@
+
+
+$(document).ready(function () {
+    $('.building-type').hide();
+
+    $("#building-type").change(function () {
+        $('.building-type').hide();
+        $("#"+ $(this).val()).show();
+    })
+    
+});  
+
+
+function Residential_Quote(){
+    let a = numFloorR
+    let b = numApartR
+    let c = numBaseR
+    result = numApartR / numFloorR
+
+
+
+    
+}
+
+function Commercial_Quote(){
+
+}
+
+function Corporate_Quote(){
+
+}
+
+function Commercial_Quote(){
+
+}
